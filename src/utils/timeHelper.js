98da5d79744311f51503a4c2494f2b747af7d530
susiechang <dwellerofuniverse@gamil.com>
2019-05-16 01:28:20 +0800

    移除moment库，使用dayjs计算时间差

--- src/utils/timeHelper.js
+++ src/utils/timeHelper.js
@@ -1,16 +1,39 @@
-import moment from 'moment'
+import dayjs from 'dayjs'
 
 export function getTimeStamp() {
   return new Date().getTime()
 }
 
-export function getTimeStr(date) {
-  moment.locale('zh-cn')
-  return moment().format('YYYY-MM-DD HH:MM:SS')
+function commonOp(dayjsObj1, dayjsObj2, key) {
+  let diff = dayjsObj1.diff(dayjsObj2, key)
+  if (diff > 0) {
+    return [true, diff]
+  }
+  return [false, diff]
 }
 
 export function getTimefromNow(dateStr) {
-  return moment(dateStr, 'YYYY-MM-DD HH:MM:SS').fromNow()
+  let dayjsObj1 = dayjs(new Date().toString()),
+    dayjsObj2 = dayjs(dateStr)
+  let tmp
+  if (commonOp(dayjsObj1, dayjsObj2, 'years')[0]) {
+    return '1年前'
+  }
+
+  if (commonOp(dayjsObj1, dayjsObj2, 'months')[0]) {
+    return '1个月前'
+  }
+
+  if (commonOp(dayjsObj1, dayjsObj2, 'day')[0]) {
+    return '1天前'
+  }
+
+  if ((tmp = commonOp(dayjsObj1, dayjsObj2, 'hours')[0])) {
+    return tmp + '小时前'
+  }
+
+  if ((tmp = commonOp(dayjsObj1, dayjsObj2, 'minutes')[0])) {
+    return tmp + '分钟前'
+  }
+  return '片刻之前'
 }
-
-
